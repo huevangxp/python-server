053e09a1bf06f29cc5f1f3a4cfda24729533aa68
huevangxp <huevang933@gmail.com>
2025-11-17 16:28:13 +0700

    commit Mon 11/17/2025 16:28:13.20

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -7,3 +7,4 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(100), unique=True, index=True)
     email = Column(String(200), unique=True)
+    password = Column(String(200))
