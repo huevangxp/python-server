fab57348ce7ee4bce71ec7bd80bc694143a4a9ce
huevangxp <huevang933@gmail.com>
2025-11-17 16:29:25 +0700

    commit Mon 11/17/2025 16:29:25.17

---
+++ app/routers/user_router.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.config.database import get_db
+from app.schemas.user_schema import UserCreate, UserResponse
+from app.services.user_service import create_user, get_users
+
+router = APIRouter(prefix="/users", tags=["Users"])
+
+@router.post("/", response_model=UserResponse)
+def create(user: UserCreate, db: Session = Depends(get_db)):
+    return create_user(db, user)
+
+@router.get("/", response_model=list[UserResponse])
+def list_users(db: Session = Depends(get_db)):
+    return get_users(db)
