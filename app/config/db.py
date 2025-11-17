04ceb73bb800c0d1fafaa0990f4f6e984f4ba425
huevangxp <huevang933@gmail.com>
2025-11-17 16:30:31 +0700

    commit Mon 11/17/2025 16:30:31.19

---
+++ app/config/db.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# PostgreSQL connection string
+DATABASE_URL = "postgresql+psycopg://postgres:password@localhost:5432/mydb"
+# If you use psycopg2:
+# DATABASE_URL = "postgresql+psycopg2://postgres:password@localhost:5432/mydb"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
